a2a8b0cb36aa8d3ec4d582f8dc30e5569ce2b1f1
Alexey Ermakov <strangebrew@yandex.ru>
2022-01-04 15:19:06 +0300

    Stream data

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Deno server-sent events demo.
 Run with
 
 `deno run --allow-net --allow-read server.ts`.
+
+Data can be sent with
+
+`curl -X GET "http://localhost:8080/send-message?message=Hello"`.
--- server.ts
+++ server.ts
@@ -1,9 +1,47 @@
-import { Application } from "https://deno.land/x/oak@v10.1.0/mod.ts";
+import { Application, Router, ServerSentEvent } from "https://deno.land/x/oak@v10.1.0/mod.ts";
 import { readableStreamFromReader } from "https://deno.land/std@0.119.0/streams/conversion.ts";
 
 const app = new Application();
+const router = new Router();
 
-app.use(async ctx => {
+let data: string | undefined;
+
+router.get("/stream", (ctx) => {
+    const target = ctx.sendEvents();
+    
+    let id = 0;
+    
+    const timer = setInterval(() => {
+		if (data !== undefined && data !== "") {
+			target.dispatchEvent(new ServerSentEvent("message", data, { id: ++id }));
+		}
+	}, 1000);
+
+	setTimeout(async () => {
+		clearInterval(timer);
+	    target.dispatchEvent(new ServerSentEvent("end-of-stream", "this is the end"));
+        await target.close();
+	}, 10000);
+});
+
+router.get("/send-message", async (ctx) => {
+    const url = new URL(ctx.request.url.href);
+    const message = url.searchParams.get("message");
+    
+    if (message === null) {
+        return;
+    }
+    
+    const target = ctx.sendEvents();
+    
+    data = message;
+
+    target.dispatchMessage(data);
+    
+    await target.close();
+});
+
+router.get("/", async (ctx) => {
     const fileStream = readableStreamFromReader(await Deno.open("index.html"));
 
     ctx.response.headers.set('Content-Type', 'text/html');
@@ -11,4 +49,6 @@ app.use(async ctx => {
 });
 
 console.log("Server started on port 8080");
+
+app.use(router.routes());
 await app.listen({ port: 8080 });
