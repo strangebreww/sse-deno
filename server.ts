412a99b2bb2b3e97f04fd75f41de733c1b693a31
Alexey Ermakov <strangebrew@yandex.ru>
2022-01-10 20:24:20 +0300

    Add event emitter, remove timers

--- server.ts
+++ server.ts
@@ -1,48 +1,76 @@
-import { Application, Router, ServerSentEvent } from "https://deno.land/x/oak@v10.1.0/mod.ts";
+import { Application, Context, ServerSentEvent } from "https://deno.land/x/oak@v10.1.0/mod.ts";
+import { EventEmitter } from "https://deno.land/x/event_emitter@1.0.0/mod.ts";
 import { readableStreamFromReader } from "https://deno.land/std@0.119.0/streams/conversion.ts";
 
-const app = new Application();
-const router = new Router();
+interface ConnectionEvents {
+	data: (data: Record<string, unknown>) => void;
+}
 
-let data: string | undefined;
+let streamCount = 0;
 
-router.get("/stream", (ctx) => {
-    const target = ctx.sendEvents();
-    
-    let id = 0;
-    
-    const timer = setInterval(() => {
-		if (data !== undefined && data !== "") {
-			target.dispatchEvent(new ServerSentEvent("message", data, { id: ++id }));
+class SSE extends EventEmitter<ConnectionEvents> {
+	constructor() {
+		super();
+	}
+
+	init(ctx: Context) {
+		const target  = ctx.sendEvents();
+
+		let id = 0;
+
+		const dataListener = (data: Record<string, unknown>) => {
+			const event = data.event && typeof data.event === "string" ? data.event : "message";
+			
+			target.dispatchEvent(new ServerSentEvent(event, data.data, { id: ++id }));
 		}
-	}, 1000);
 
-	setTimeout(async () => {
-		clearInterval(timer);
-	    target.dispatchEvent(new ServerSentEvent("end-of-stream", "this is the end"));
-        await target.close();
-	}, 10000);
+		this.on("data", dataListener);
+
+		target.addEventListener("close", () => {
+			this.off("data", dataListener);
+
+			--streamCount;
+			
+			console.log("Stream closed");
+		});
+
+		console.log(`Stream ${++streamCount} created`);
+	}
+
+	send(data: Record<string, unknown>) {
+		this.emit("data", data);
+	}
+}
+
+const app = new Application();
+const sse = new SSE();
+
+app.use(async (ctx, next) => {
+	if (ctx.request.url.pathname === "/stream") {
+		sse.init(ctx);
+
+		return;
+	}
+
+	if (ctx.request.url.pathname === "/send-message") {
+		const url = new URL(ctx.request.url.href);
+		const message = url.searchParams.get("message");
+		
+		if (message === null) {
+			return;
+		}
+		
+		sse.send({ data: message });
+
+		return;
+	}
+
+	await next();
 });
+  
 
-router.get("/send-message", async (ctx) => {
-    const url = new URL(ctx.request.url.href);
-    const message = url.searchParams.get("message");
-    
-    if (message === null) {
-        return;
-    }
-    
-    const target = ctx.sendEvents();
-    
-    data = message;
-
-    target.dispatchMessage(data);
-    
-    await target.close();
-});
-
-router.get("/", async (ctx) => {
-    const fileStream = readableStreamFromReader(await Deno.open("index.html"));
+app.use(async (ctx) => {
+	const fileStream = readableStreamFromReader(await Deno.open("index.html"));
 
     ctx.response.headers.set('Content-Type', 'text/html');
     ctx.response.body = fileStream;
@@ -50,5 +78,4 @@ router.get("/", async (ctx) => {
 
 console.log("Server started on port 8080");
 
-app.use(router.routes());
 await app.listen({ port: 8080 });
